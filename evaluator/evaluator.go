d2961ee6a12b1eed84f45ac7b08924f57d9b7859
V-Mokhun <mohunmax1408@gmail.com>
2024-05-11 13:27:06 +0200

    feat: evaluate prefix expressions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -3,6 +3,7 @@ package evaluator
 import (
 	"github.com/v-mokhun/golang-interpreter/ast"
 	"github.com/v-mokhun/golang-interpreter/object"
+	"github.com/v-mokhun/golang-interpreter/token"
 )
 
 var (
@@ -17,6 +18,10 @@ func Eval(node ast.Node) object.Object {
 		return evalStatements(node.Statements)
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
+
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 
 	case *ast.IntegerLiteral:
 		return &object.Integer{
@@ -38,9 +43,43 @@ func evalStatements(stmts []ast.Statement) object.Object {
 
 	return result
 }
+
 func nativeBoolToBooleanObject(input bool) *object.Boolean {
 	if input {
 		return TRUE
 	}
 	return FALSE
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case token.BANG:
+		return evalBangOperatorExpression(right)
+	case token.MINUS:
+		return evanMinusPrefixOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
+}
+
+func evanMinusPrefixOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
 }
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -52,7 +52,10 @@ func TestEvalIntegerExpression(t *testing.T) {
 		expected int64
 	}{
 		{"5", 5},
-		{"10", 10}}
+		{"10", 10},
+		{"-5", -5},
+		{"-10", -10},
+	}
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
 		testIntegerObject(t, evaluated, tt.expected)
@@ -66,6 +69,24 @@ func TestEvalBooleanExpression(t *testing.T) {
 	}{
 		{"true", true},
 		{"false", false}}
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
+func TestBangOperator(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"!true", false},
+		{"!false", true},
+		{"!5", false},
+		{"!!true", true},
+		{"!!false", false},
+		{"!!5", true},
+	}
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
 		testBooleanObject(t, evaluated, tt.expected)
