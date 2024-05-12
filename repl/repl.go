d5ab4017e75fbeee08cb67a1de663e61f462e472
V-Mokhun <mohunmax1408@gmail.com>
2024-05-12 18:19:27 +0200

    feat: evaluate let statements; identifier bindings

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/v-mokhun/golang-interpreter/evaluator"
 	"github.com/v-mokhun/golang-interpreter/lexer"
+	"github.com/v-mokhun/golang-interpreter/object"
 	"github.com/v-mokhun/golang-interpreter/parser"
 )
 
@@ -14,6 +15,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Print(PROMPT)
@@ -32,7 +34,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
