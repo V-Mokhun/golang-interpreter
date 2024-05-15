f133b0d9ab9d9a9300af161818e8b2239fcde602
V-Mokhun <mohunmax1408@gmail.com>
2024-05-15 12:22:33 +0200

    feat: builitin function `len`

---
+++ evaluator/builtins.go
@@ -0,0 +1,22 @@
+package evaluator
+
+import "github.com/v-mokhun/golang-interpreter/object"
+
+var builtins = map[string]*object.Builtin{
+	"len": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+
+			str, ok := args[0].(*object.String)
+			if !ok {
+				return newError("argument to `len` not supported, got %s", args[0].Type())
+			}
+
+			return &object.Integer{
+				Value: int64(len(str.Value)),
+			}
+		},
+	},
+}
